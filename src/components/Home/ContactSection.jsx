065632287c3d30aa54a9f34e1c1c8d12c21c3174
FajlaRabby24 <fajlarabbyla24@gmail.com>
2025-09-09 12:48:14 +0600

    contact section border color change and add some padding

--- src/components/Home/ContactSection.jsx
+++ src/components/Home/ContactSection.jsx
@@ -123,122 +123,128 @@ const ContactSection = () => {
   };
 
   return (
-    <Container className="border border-info  rounded-2xl py-3 relative mb-40">
+    <Container className="border border-[#cccccc8e]  rounded-2xl py-3 relative mb-40">
       <span className="text-lg font-medium absolute -top-6 left-1/2 -translate-x-1/2 text-center mb-6 bg-primary  py-2 px-5 rounded-full">
         Our Branches
       </span>
 
-      {/* Branch Tabs */}
-      <div className="flex flex-wrap justify-center mt-12 gap-4 mb-6">
-        {branches.map((branch) => (
-          <button
-            key={branch.id}
-            onClick={() => setActiveBranch(branch)}
-            className={`px-4 py-2 rounded-lg cursor-pointer border transition flex flex-col items-center justify-center ${
-              activeBranch.id === branch.id
-                ? "bg-lime-100 "
-                : "bg-white border-gray-300 hover:bg-gray-100"
-            }`}
-          >
-            <img
-              className="min-h-8 max-h-8"
-              src={branch.image}
-              alt={branch.name + " image"}
-            />
-            {branch.name}
-          </button>
-        ))}
-      </div>
-
-      {/* Grid Layout */}
-      <div className="grid md:grid-cols-2 gap-6">
-        {/* Contact Form */}
-        <div className="bg-radial-[at_550%_550%] from-info to-neutral text-white p-6 rounded-2xl shadow-md">
-          <h3 className="text-xl font-semibold mb-4">Contact Us</h3>
-          <form
-            ref={formRef}
-            className="space-y-4"
-            onSubmit={handleSubmit(sendEmail)}
-          >
-            <input
-              type="text"
-              placeholder="Your Name"
-              {...register("user_name", { required: true })}
-              className="input input-bordered w-full bg-[#1a4267] placeholder:text-white"
-            />
-            {errors.user_name && (
-              <span className="text-red-300 text-sm">Name is required</span>
-            )}
-
-            <input
-              type="email"
-              placeholder="Your Email"
-              {...register("user_email", { required: true })}
-              className="input input-bordered w-full bg-[#1a4267] placeholder:text-white"
-            />
-            {errors.user_email && (
-              <span className="text-red-300 text-sm">Email is required</span>
-            )}
-
-            <input
-              type="text"
-              placeholder="Phone (optional)"
-              {...register("user_phone")}
-              className="input input-bordered w-full bg-[#1a4267] placeholder:text-white"
-            />
-
-            <input
-              type="text"
-              placeholder="Subject"
-              {...register("user_subject", { required: true })}
-              className="input input-bordered w-full bg-[#1a4267] placeholder:text-white"
-            />
-            {errors.user_subject && (
-              <span className="text-red-300 text-sm">Subject is required</span>
-            )}
-
-            <textarea
-              placeholder="Message..."
-              {...register("message", { required: true })}
-              className="textarea textarea-bordered w-full bg-[#1a4267] placeholder:text-white"
-            ></textarea>
-            {errors.message && (
-              <span className="text-red-300 text-sm">Message is required</span>
-            )}
-
+      <div className="xl:px-3  ">
+        {/* Branch Tabs */}
+        <div className="flex flex-wrap justify-center mt-12 gap-4 mb-6">
+          {branches.map((branch) => (
             <button
-              type="submit"
-              disabled={isSubmitting}
-              className={`btn w-full ${
-                isSubmitting ? "cursor-not-allowed" : ""
-              } btn-primary text-black`}
+              key={branch.id}
+              onClick={() => setActiveBranch(branch)}
+              className={`px-4 py-2 rounded-lg cursor-pointer border transition flex flex-col items-center justify-center ${
+                activeBranch.id === branch.id
+                  ? "bg-lime-100 "
+                  : "bg-white border-gray-300 hover:bg-gray-100"
+              }`}
             >
-              {isSubmitting ? (
-                <span className="loading loading-spinner loading-md"></span>
-              ) : (
-                "Send Email"
-              )}
+              <img
+                className="min-h-8 max-h-8"
+                src={branch.image}
+                alt={branch.name + " image"}
+              />
+              {branch.name}
             </button>
-          </form>
+          ))}
         </div>
 
-        {/* Map */}
-        <div className="h-[300px] sm:h-[400px] md:h-auto w-full rounded-2xl overflow-hidden shadow-md">
-          <MapContainer
-            center={activeBranch.coords}
-            zoom={13}
-            scrollWheelZoom={true}
-            className="h-full w-full"
-            key={activeBranch.id}
-          >
-            <TileLayer
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              attribution="&copy; OpenStreetMap contributors"
-            />
-            <Marker position={activeBranch.coords}>
-              <Popup>{activeBranch.address}</Popup>
-            </Marker>
-          </MapContainer>
+        {/* Grid Layout */}
+        <div className="grid md:grid-cols-2 gap-6">
+          {/* Contact Form */}
+          <div className="bg-radial-[at_550%_550%] from-info to-neutral text-white p-6 rounded-2xl shadow-md">
+            <h3 className="text-xl font-semibold mb-4">Contact Us</h3>
+            <form
+              ref={formRef}
+              className="space-y-4"
+              onSubmit={handleSubmit(sendEmail)}
+            >
+              <input
+                type="text"
+                placeholder="Your Name"
+                {...register("user_name", { required: true })}
+                className="input input-bordered w-full bg-[#1a4267] placeholder:text-white"
+              />
+              {errors.user_name && (
+                <span className="text-red-300 text-sm">Name is required</span>
+              )}
+
+              <input
+                type="email"
+                placeholder="Your Email"
+                {...register("user_email", { required: true })}
+                className="input input-bordered w-full bg-[#1a4267] placeholder:text-white"
+              />
+              {errors.user_email && (
+                <span className="text-red-300 text-sm">Email is required</span>
+              )}
+
+              <input
+                type="text"
+                placeholder="Phone (optional)"
+                {...register("user_phone")}
+                className="input input-bordered w-full bg-[#1a4267] placeholder:text-white"
+              />
+
+              <input
+                type="text"
+                placeholder="Subject"
+                {...register("user_subject", { required: true })}
+                className="input input-bordered w-full bg-[#1a4267] placeholder:text-white"
+              />
+              {errors.user_subject && (
+                <span className="text-red-300 text-sm">
+                  Subject is required
+                </span>
+              )}
+
+              <textarea
+                placeholder="Message..."
+                {...register("message", { required: true })}
+                className="textarea textarea-bordered w-full bg-[#1a4267] placeholder:text-white"
+              ></textarea>
+              {errors.message && (
+                <span className="text-red-300 text-sm">
+                  Message is required
+                </span>
+              )}
+
+              <button
+                type="submit"
+                disabled={isSubmitting}
+                className={`btn w-full ${
+                  isSubmitting ? "cursor-not-allowed" : ""
+                } btn-primary text-black`}
+              >
+                {isSubmitting ? (
+                  <span className="loading loading-spinner loading-md"></span>
+                ) : (
+                  "Send Email"
+                )}
+              </button>
+            </form>
+          </div>
+
+          {/* Map */}
+          <div className="h-[300px] sm:h-[400px] md:h-auto w-full rounded-2xl overflow-hidden shadow-md">
+            <MapContainer
+              center={activeBranch.coords}
+              zoom={13}
+              scrollWheelZoom={true}
+              className="h-full w-full"
+              key={activeBranch.id}
+            >
+              <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution="&copy; OpenStreetMap contributors"
+              />
+              <Marker position={activeBranch.coords}>
+                <Popup>{activeBranch.address}</Popup>
+              </Marker>
+            </MapContainer>
+          </div>
         </div>
       </div>
     </Container>
--- src/components/common/Container.jsx
+++ src/components/common/Container.jsx
@@ -1,6 +1,6 @@
 const Container = ({ children, className }) => {
   return (
-    <div className={`${className} max-w-7xl  mx-auto px-3 xl:px-0`}>
+    <div className={` max-w-7xl  mx-auto px-3 xl:px-0 ${className}`}>
       {children}
     </div>
   );
